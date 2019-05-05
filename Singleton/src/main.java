1f5df446f280f44014c178d1a05bb8c1bb736e86
azord <azord@DESKTOP-LJQ163H>
2019-05-05 22:26:04 +0200

    Version sin error

--- Singleton/src/main.java
+++ Singleton/src/main.java
@@ -8,13 +8,13 @@ public class main {
         
         try{
             Unicon richard = ricardo.clone();
-            System.out.println(richard.getNombre());
+            System.out.println(richard);
         }catch (NullPointerException ex){
             ex.printStackTrace();
         }
         
         // ricardo y ramon son referencias a un único objeto de la clase SoyUnico
-        System.out.println(ramon.getNombre());
-        System.out.println(ricardo.getNombre());   
+        System.out.println(ramon);
+        System.out.println(ricardo);   
     }
 }
